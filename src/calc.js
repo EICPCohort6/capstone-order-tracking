ba4a6501b29c950cf996cd50bf4116a048dc6eba
Jdrinkwater1 <60936783+Jdrinkwater1@users.noreply.github.com>
2022-08-16 15:39:06 -0400

    Not supposed to be on main

--- src/calc.js
+++
@@ -1,5 +0,0 @@
-function calculator(a, b) {
-    return a+b;
-}
-
-console.log(calculator(4, 5));
--- src/test/test.js
+++
@@ -1,7 +0,0 @@
-function calculator(a, b) {
-    return a+b;
-}
-
-const foo = () => {
-    return calculator(4, 5) === 9;
-}
