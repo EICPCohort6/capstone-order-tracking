2bb41135503a22038b4e3f06ad4c446aaeadeaa7
Sara Anwer <sar07997@tjx.com>
2022-08-17 12:04:38 -0400

    you better fail

--- Test/dummyTest.js
+++ Test/dummyTest.js
@@ -1,5 +1,5 @@
 let assert = require('assert');
 
 it('should return -1 when the value is not present', function() {
-    assert.equal(-1, [1,2,3].indexOf(4)); //-1 === -1
+    assert.equal(-2, [1,2,3].indexOf(4)); //-1 === -1
 });
