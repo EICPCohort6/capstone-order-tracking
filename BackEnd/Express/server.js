5d7290feda2e9f223960e2a196020095f16d862a
Uzi Ahmad <usa00014@tjx.com>
2022-08-17 14:55:52 +0100

    initialized connection with sequelize, and updated server.js

--- BackEnd/Express/server.js
+++ BackEnd/Express/server.js
@@ -4,22 +4,39 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-    origin: "http://localhost:8080"
-}
+  origin: "http://localhost:8080",
+};
 
 app.use(cors(corsOptions));
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
+
+//init sequelize and run sync
+const connection = require(sequelize);
+
+(async function () {
+  try {
+    await connection.sync({
+      logging: console.log,
+      force: true, // use this to resync database in development, otherwise leave when production
+    });
+    console.log("Successful connection");
+  } catch (err) {
+    console.error("Database error:", err);
+  }
+})();
 
 // test endpoint
 /*
 app.get("/", (req, res) => {
-    res.json({ message: "Backend of capstone order tracking is up and running!" });
+  res.json({
+    message: "Backend of capstone order tracking is up and running!",
+  });
 });
 */
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}.`);
+  console.log(`Server is running on port ${PORT}.`);
 });
