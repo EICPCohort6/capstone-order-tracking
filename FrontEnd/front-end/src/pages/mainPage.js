6646613257d6d7bc7130939839e5bd02052c8439
Patrick McGonagle <pat04531@tjx.com>
2022-08-17 15:59:13 -0400

    Minor updates

--- FrontEnd/front-end/src/pages/mainPage.js
+++ FrontEnd/front-end/src/pages/mainPage.js
@@ -5,9 +5,16 @@ import CustomerForm from "../components/create-customer";
 const MainPage = () => {
   return (
     <>
-      <h1>[header/logo]</h1>
-      <Container>
-        <CustomerForm />
+      <h1>header/logo</h1>
+      <Container fluid>
+        <Row>
+          <Col xs="3">
+            <h1>NAVBAR HERE</h1>
+          </Col>
+          <Col xs="6">
+            <CustomerForm />
+          </Col>
+        </Row>
       </Container>
     </>
   );
