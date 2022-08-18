d014e94236b626a7b1d013deaa3d6ec872f6cd61
Justas Labeikis <jus03242@tjx.com>
2022-08-18 13:22:40 +0100

    Changed padding

--- FrontEnd/front-end/src/components/navigation.js
+++ FrontEnd/front-end/src/components/navigation.js
@@ -9,7 +9,7 @@ const NavigationBar = () => {
           <img src="/assets/placeholder_template.jpg" />
         </Row>
         <Row className="px-4">
-          <Col xs="4" noGutters>
+          <Col xs="12" md="4" noGutters>
             <img
               src="/assets/default-profile.jpg"
               width="100%"
@@ -17,11 +17,11 @@ const NavigationBar = () => {
               className="User-image"
             />
           </Col>
-          <Col xs="8" className="User-name p-2">
+          <Col xs="12" md="8" className="User-name p-2">
             <p>Username</p>
           </Col>
         </Row>
-        <Row className="p-4 Color-nav">
+        <Row className="p-0 Color-nav">
           <Nav pills vertical>
             <NavItem className="p-1">
               <NavLink active href="#">
