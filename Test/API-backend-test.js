4c74ccd604ed53850453e368524e38ff0101b2aa
Petros Filiogiannis <petros_filiogiannis@tjx.com>
2022-08-18 12:04:08 -0400

    added a test to API-backend-test.js

--- Test/API-backend-test.js
+++ Test/API-backend-test.js
@@ -13,16 +13,43 @@ describe('GET /api/customers', function() {
             .expect(200, done);
     });
 
+    it('responds with 404 not found', function(done) {
+        request(app)
+            .get('blahblah')
+            .set('Accept', 'application/json')
+            //.expect('Content-Type', /json/)
+            .expect(404, done);
+    });
+
     it('customer ID 1 first_name is Zara', function(done) {
         request(app)
             .get('/api/customers/1')
             .set('Accept', 'application/json')
             .expect('Content-Type', /json/)
             .expect(200, {
-                id: 1,
-                first_name: 'Zara'
+                customer_id: 1,
+                first_name: 'Zara',
+                middle_name: 'El',
+                last_name: 'Alaoui',
+                phone_number: '781-816-0325',
+                email: 'zara_elalaoui@tjx.com',
+                customer_notes: 'No Notes',
+                street_number: 21,
+                unit_number: 'Apt 2A',
+                street_name: 'Smith Street',
+                city: 'Boston',
+                state: 'MA',
+                country: 'US',
+                zipcode: '02125'
             }, done);
     });
+
+    // it('customer ID "hello" returns 400 bad request', function(done) {
+    //     request(app)
+    //         .get(`/api/customers/"hello"`)
+    //         .set('Accept', 'application/json')
+    //         .expect(400, done)
+    // });
 
 });
 
