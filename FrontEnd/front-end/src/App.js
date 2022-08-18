109dc0cd3ccda9c3d72163552980261585903811
Justas Labeikis <jus03242@tjx.com>
2022-08-18 15:23:21 +0100

    Added a 404 page

--- FrontEnd/front-end/src/App.js
+++ FrontEnd/front-end/src/App.js
@@ -3,6 +3,7 @@ import MainPage from "./pages/mainPage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import CustomerPage from "./pages/customersPage";
 import OrderPage from "./pages/ordersPage";
+import NoPage from "./pages/noPage";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<MainPage />}>
           <Route index element={<CustomerPage />} />
           <Route path="order" element={<OrderPage />} />
+          <Route path="*" element={<NoPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
