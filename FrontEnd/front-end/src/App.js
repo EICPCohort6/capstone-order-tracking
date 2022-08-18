6d5829afda42a4347717fd3ae9ad86fa6c3c1a3c
Alex Charpentier <alexander_charpentier01@tjx.com>
2022-08-18 11:26:21 -0400

    Test 2

--- FrontEnd/front-end/src/App.js
+++ FrontEnd/front-end/src/App.js
@@ -1,8 +1,8 @@
 
-import "./App.css";\
+import "./App.css";
 import React from "react";
 import MainPage from "./pages/mainPage";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 import CustomerPage from "./pages/customersPage";
 import OrderPage from "./pages/ordersPage";
 import NoPage from "./pages/noPage";
