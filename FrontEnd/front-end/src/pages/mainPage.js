d5193c6d400ed28d8fbaa6101423d276e2af4907
fanimamofa <109087431+fanimamofa@users.noreply.github.com>
2022-08-18 23:39:01 -0400

    form revised to use reactstrap

--- FrontEnd/front-end/src/pages/mainPage.js
+++ FrontEnd/front-end/src/pages/mainPage.js
@@ -1,18 +1,22 @@
 import React from "react";
 import { Row, Container, Col } from "reactstrap";
-import OrdersPage from "./ordersPage";
+import CustomerForm from "./customerForm";
 const MainPage = () => {
   return (
     <>
       <h1>header/logo</h1>
       <Container fluid>
-        <Row>
+           <Col xs="8">
+            <h1>BASE CONTENT HERE</h1>
+          </Col> <Row>
           <Col xs="4">
             <h1>NAVBAR HERE</h1>
           </Col>
           <Col xs="8">
             <h1>BASE CONTENT HERE</h1>
-            <OrdersPage/>
+          </Col>
+          <Col xs="8">
+            <CustomerForm/>
           </Col>
 
         </Row>
--- FrontEnd/front-end/src/pages/ordersPage.js
+++ FrontEnd/front-end/src/pages/ordersPage.js
@@ -1,76 +1,9 @@
-import React, {useState } from "react";
+
 
 const OrdersPage = () => {
-    const [orderId, setOrderId] = useState(null);
-    const [orderStatus, setOrderStatus] = useState(null);
-    const [customerId, setCustomerId] = useState(null);
-    const [total, setTotal] = useState(0);
-    const [orderNotes, setOrderNotes] = useState(null)
-
-    const handleSubmit = () => {
-        const orderObject = {
-            orderId,
-            orderStatus,
-            customerId,
-            total,
-            orderNotes,
-            timestamp: Date.now()
-        }
-        console.log(orderObject);
-        const orderString = JSON.stringify(orderObject);
-        alert("Order was submitted with the following details: " + orderString)
-    }
     return (
       <>
-        <h1>JJ</h1>
-<div>
-<div class="field">
-  <label class="label">Order Id</label>
-  <div class="control">
-    <input class="input" type="text" value={orderId} onChange={(event) => {
-        setOrderId(event.target.value)
-    }} placeholder="Text input" />
-  </div>
-</div>
-<div class="field">
-  <label class="label">Order status</label>
-  <div class="control">
-    <input class="input" type="text" placeholder="Text input" value={orderStatus} onChange={(event) => {
-        setOrderStatus(event.target.value)
-    }} />
-  </div>
-</div>
-<div class="field">
-  <label class="label">Customer Id</label>
-  <div class="control">
-    <input class="input" type="text" placeholder="Text input" value={customerId} onChange={(event) => {
-        setCustomerId(event.target.value)
-    }}  />
-  </div>
-</div>
-<div class="field">
-  <label class="label">Total</label>
-  <div class="control">
-    <input class="input" type="text" placeholder="Total Amount" value={total} onChange={(event) => {
-        setTotal(event.target.value)
-    }} />
-  </div>
-</div>
-
-<div class="field">
-  <label class="label">Order Notes</label>
-  <div class="control">
-    <textarea class="textarea" placeholder="Textarea" value={orderNotes} onChange={(event) => {
-        setOrderNotes(event.target.value)
-    }} />
-  </div>
-</div>
-
-<div class="control">
-  <button class="button is-primary" onClick={() => handleSubmit()}>Submit</button>
-</div>
-
-</div>
+      This is my order page
       </>
     );
   };
