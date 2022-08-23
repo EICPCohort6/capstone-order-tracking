41bcd3965483e580033d2642ad7a40f769ae6cdc
Alex Charpentier <alexander_charpentier01@tjx.com>
2022-08-23 12:00:09 -0400

    Revert "Moved Front End Folders into root"
    This reverts commit 837951672b5b6c880f4cfee8b44044327c511dcb.

---
+++ FrontEnd/front-end/src/components/navigation.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { Row, Container, Col, Nav, NavItem, NavLink } from "reactstrap";
+import { Link } from "react-router-dom";
+
+const NavigationBar = () => {
+  return (
+    <>
+      <Container className="p-0">
+        <img style={{ width: "100%" }} src="/assets/placeholder_template.jpg" alt=""/>
+
+        <Row className="px-4">
+          <Col xs="12" md="4" noGutters>
+            <img
+              src="/assets/default-profile.jpg"
+              width="100%"
+              height="100%"
+              className="User-image"
+              alt=""
+            />
+          </Col>
+          <Col xs="12" md="8" className="User-name p-2">
+            <p>Username</p>
+          </Col>
+        </Row>
+        <Row className="p-0 Color-nav">
+          <Nav pills vertical>
+            <NavItem className="p-1">
+              <NavLink active href="#">
+                Orders
+              </NavLink>
+            </NavItem>
+            <NavItem className="p-1">
+              <Link to={"/"}>Customers</Link>
+            </NavItem>
+            <NavItem className="p-1">
+              <Link to={"order"}>Orders</Link>
+            </NavItem>
+          </Nav>
+        </Row>
+      </Container>
+    </>
+  );
+};
+
+export default NavigationBar;
