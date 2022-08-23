ddffb78d80e6c06af7cfb6df7ea7647d854a1b44
Petros Filiogiannis <petros_filiogiannis@tjx.com>
2022-08-23 16:17:18 -0400

    made test skeleton

--- Test/frontend-backend.test.js
+++ Test/frontend-backend.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from '../src/App';
 
 test('renders Search bar default text', () => {
   render(<App />);
