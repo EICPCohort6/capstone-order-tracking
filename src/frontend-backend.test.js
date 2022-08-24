c2e60d3d3b06898f19e2c86b0172bedf12e3e45d
Petros Filiogiannis <petros_filiogiannis@tjx.com>
2022-08-24 11:19:46 -0400

    started on second frontend int test

--- src/frontend-backend.test.js
+++ src/frontend-backend.test.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import user from "@testing-library/user-event";
 import App from './App';
 
-test('renders Search bar default text', () => {
+test('renders Search button', () => {
   render(<App />);
   //const linkElement = screen.getByText(/Getting started with React testing library/i);
   expect(screen.getByText("Search")).toBeInTheDocument();
+});
+
+test('Searches customer by last name Prince', () => {
+  render(<App />);
+  const searchButton = screen.getByText("Search"); //get search button
+  user.type(screen.getByPlaceholderText("Search"), "Prince"); //get search bar element & type "Prince"
+  expect(searchButton).toBeEnabled();
+  user.click(searchButton);
+
 });
