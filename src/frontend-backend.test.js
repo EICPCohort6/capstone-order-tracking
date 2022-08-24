43472fbe1763b9ab10877faa964ecf1f2d0c6b86
Petros Filiogiannis <petros_filiogiannis@tjx.com>
2022-08-24 15:42:53 -0400

    updated frontend backend tests

--- src/frontend-backend.test.js
+++ src/frontend-backend.test.js
@@ -2,6 +2,11 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import user from "@testing-library/user-event";
 import App from './App';
+jest.mock("../BackEnd/Express/server");
+
+// const testing = screen.getByText((content, element) => {
+//   return element.tagName.toLowerCase() === 'td' && content.startsWith('M')});
+// console.log(testing);
 
 test('renders Search button', () => {
   render(<App />);
@@ -9,11 +14,17 @@ test('renders Search button', () => {
   expect(screen.getByText("Search")).toBeInTheDocument();
 });
 
-test('Searches customer by last name Prince', () => {
+test('Searches customer by last name Prince', async () => {
   render(<App />);
   const searchButton = screen.getByText("Search"); //get search button
   user.type(screen.getByPlaceholderText("Search"), "Prince"); //get search bar element & type "Prince"
   expect(searchButton).toBeEnabled();
   user.click(searchButton);
 
+  expect(screen.getByText("First Name")).toBeInTheDocument();
+  expect(await screen.findByText("Yoshi")).toBeInTheDocument();
+  //expect(screen.getByText((content, element) => {
+  //  return element.tagName.toLowerCase() === 'td' && content.startsWith('M')})).toBeInTheDocument();
+  //expect(screen.getByText("Yoshi")).toBeInTheDocument();
+  //expect(screen.getByText("consequat@protonmail.com")).toBeInTheDocument();
 });
