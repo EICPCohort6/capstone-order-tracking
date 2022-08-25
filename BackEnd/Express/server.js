93a39da2cf247fb0b17d36b711192f0e83f19b72
alcharpen <109087396+alcharpen@users.noreply.github.com>
2022-08-25 09:38:44 -0400

    Update server.js

--- BackEnd/Express/server.js
+++ BackEnd/Express/server.js
@@ -39,11 +39,5 @@ require("../SQL/Routes/customer-routes")(app);
 require("../SQL/Routes/order-routes")(app);
 require("../SQL/Routes/product-routes")(app);
 
-
-// set port, listen for requests
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
-});
 module.exports = app;
 
