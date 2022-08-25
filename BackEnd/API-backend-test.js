893310e0237eb53ef10433320bd5f72d5053c035
Alex Charpentier <alexander_charpentier01@tjx.com>
2022-08-25 11:10:10 -0400

    Cleaned Directory

--- BackEnd/API-backend-test.js
+++ BackEnd/API-backend-test.js
@@ -2,7 +2,7 @@ const request = require("supertest");
 const assert = require("assert");
 const express = require("express");
 
-const app = require("../test-app/app.js"); // express();
+const app = require("./app.js"); // express();
 
 describe("tests for /api/customers", function () {
   //tests for GET requests
