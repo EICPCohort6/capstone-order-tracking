da1989bd22944154390de87f118e3020c2ae9e46
Alexis Bagramian <alexisbagramian@gmail.com>
2022-08-26 09:23:13 -0400

    url out of date

---
+++ FrontEnd/src/pages/customersPage.js
@@ -0,0 +1,95 @@
+import React, { useState } from "react";
+import CustomerSearch from "../components/customerSearch";
+import CustomerTableDisplay from "../components/customerTable";
+import axios from "axios";
+import AddCustomerButton from "../components/add-customer-button";
+
+const getData = async ({ condition, text }) => {
+  // does api call gets data
+  let customer;
+  switch (condition) {
+    case "Last Name":
+      const customer = await axios.get(
+        `http://localhost:8080/api/customers?last_name=${text}`
+      );
+      return customer;
+    case "First Name":
+      customer = await axios.get(
+        `http://localhost:8080/api/customers?first_name=${text}`
+      );
+      return customer;
+    case "ID":
+      customer = await axios.get(`http://localhost:8080/api/customers/${text}`);
+      return customer;
+    case "Phone Number":
+      customer = await axios.get(
+        `http://localhost:8080/api/customers?phone_number=${text}`
+      );
+      return customer;
+    case "Email":
+      customer = await axios.get(
+        `http://localhost:8080/api/customers?email=${text}`
+      );
+      return customer;
+
+    default:
+      alert("No condition selected!");
+      return "empty";
+  }
+};
+const createNewCustomer = async (person) => {
+  axios.post(`http://localhost:8080/api/customers`, person).then((result) => {
+    alert("Customer added!");
+    window.location.reload();
+  });
+};
+
+const CustomerPage = () => {
+  const [tableData, setTableData] = useState("empty");
+
+  // Deletes a entry from the table
+  const deleteItem = async (id) => {
+    await axios.delete(`http://localhost:8080/api/customers/${id}`).then(() => {
+      const newTable = tableData.filter(
+        (row) => row.displayData.customer_id !== id
+      );
+      setTableData(newTable);
+    });
+  };
+
+  const updateItem = async (item) => {
+    await axios
+      .put(`https://capstone-csr-api.azurewebsites.net/api/customers/${item.customer_id}`, item)
+      .then(() => {
+        const newTable = tableData.map((row) => {
+          if (row.displayData.customer_id !== item.customer_id) return row;
+          const newDisplayData = {
+            customer_id: item.customer_id,
+            first_name: item.first_name,
+            middle_name: item.middle_name,
+            last_name: item.last_name,
+            phone_number: item.phone_number,
+            email: item.email,
+            country: item.country,
+            city: item.city,
+          };
+          return { displayData: newDisplayData, fullData: item };
+        });
+        setTableData(newTable);
+      });
+  };
+
+  return (
+    <>
+      <CustomerSearch getData={getData} setTableData={setTableData} />
+      <AddCustomerButton customerFunction={createNewCustomer} />
+      <CustomerTableDisplay
+        tableData={tableData}
+        deleteItem={deleteItem}
+        updateItem={updateItem}
+      />
+    </>
+  );
+};
+
+export default CustomerPage;
